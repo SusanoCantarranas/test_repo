44cad52d806eeeedd6d4e78ab3279d2e2c108253
SusanoCantarranas <susano.cantarranas@gmail.com>
2023-01-30 16:27:18 -0600

    Creacion de clase Cliente

---
+++ Cliente.java
@@ -0,0 +1,14 @@
+public class Cliente{
+
+    private String Nombre;
+    private Integer Edad;
+
+    public Cliente(pNombre String, pEdad Integer){
+        Nombre = pNombre;
+        Edad = pEdad;
+    }
+
+    public String getNombre{ return Nombre;}
+    public Integer getEdad{ return Edad;}
+
+}
