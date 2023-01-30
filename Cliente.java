f839fa12267514709ebe9db7569716d7e9337611
SusanoCantarranas <susano.cantarranas@gmail.com>
2023-01-30 16:31:04 -0600

    Se agregaron parametros de apellidos al constructor

--- Cliente.java
+++ Cliente.java
@@ -1,10 +1,14 @@
 public class Cliente{
 
     private String Nombre;
+    private String ApPaterno;
+    private String ApMaterno;
     private Integer Edad;
 
-    public Cliente(pNombre String, pEdad Integer){
+    public Cliente(String pNombre, String pApPat, String pApMat,Integer pEdad){
         Nombre = pNombre;
+        ApPaterno = pApMat;
+        ApMaterno = pApMat;
         Edad = pEdad;
     }
 
